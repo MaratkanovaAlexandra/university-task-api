1c12f7861b507d112f2082230c7375921878e993
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-07 15:31:26 +0400

    feat: create method witch senging requests to db

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var Connection = require('tedious').Connection;  
+var Request = require('tedious').Request;
+
 var config = {  
   server: 'localhost', 
   authentication: {
@@ -15,8 +17,34 @@ var config = {
 };  
 var connection = new Connection(config);  
 connection.on('connect', function(err) {  
-  if (err) console.log(`Error: ${err.message}`);
-  else console.log("Connected");  
+  if (err) throw err;
+
+  console.log("Connected");  
+  executeStatement();
 });
 
 connection.connect();
+
+function executeStatement() {
+  const request = new Request('SELECT * FROM Persons', (err) => {
+    console.log("Start Request")
+    if (err) {
+      throw err;
+    }
+
+    console.log('DONE!');
+    connection.close();
+  });
+
+  request.on('row', (columns) => {
+    columns.forEach((column) => {
+      if (column.value === null) {
+        console.log('NULL');
+      } else {
+        console.log(column.value);
+      }
+    });
+  });
+
+  connection.execSql(request);  
+}
